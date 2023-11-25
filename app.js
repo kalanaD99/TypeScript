4f4aa2d48fb5468533f2a98ab7fd0d8cd138eee4
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-11-25 12:00:52 +0530

    Primitive type done

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ var value1 = sum(+n1, +n2);
 console.log(value1);
 var value2 = sum(+n1, +n2);
 console.log(value2);
+//primitive number String Boolean
+function sendSum(num1, num2, name, isWithName) {
+    var result = isWithName ? "Hi ".concat(name, ", Sum is ").concat(num1 + num2) : "Sum is ".concat(num1 + num2);
+    return result;
+}
+var num1 = 10;
+var num2 = 10;
+var myName = 'Kalana';
+var withName = true;
+var result1 = sendSum(10, 20, 'Kalana', true);
+console.log(result1);
+var result2 = sendSum(10, 20, 'Kalana', false);
+console.log(result2);
