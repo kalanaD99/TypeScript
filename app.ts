2a3429a215a978eaea5f56731496d2975bd0efb8
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-11-25 12:25:21 +0530

    function with type guard

--- app.ts
+++ app.ts
@@ -14,7 +14,8 @@ console.log(value1);
 let value2 = sum(+n1,+n2);
 console.log(value2);
 
-//primitive number String Boolean
+           //primitive number String Boolean
+//-------------------------------------------------//
 function sendSum(num1: number, num2: number, name: string, isWithName: boolean){
     let result = isWithName ? `Hi ${name}, Sum is ${num1 + num2}` : `Sum is ${num1 + num2}`;
     return result;
@@ -30,13 +31,31 @@ console.log(result1);
 let result2= sendSum(10, 20,'Kalana', false);
 console.log(result2);
 
-//functions
+            //functions
+//----------------------------------//
+
 //function findSum(num1: number,num2: number):number(return wenna ona type ek define krnawa){
 //return num1+num2+"" (methanin define krnne return wenna ona type eka );
+//return `sum is ${num1+num2}`; string type ek
 
 function findSum(num1: number,num2: number){
     return num1+num2;
 }
+/*function findTheSum(num1: number, num2: number){
+    return `Sum is ${num1+num2}`;
+}*/
 
+function findTheSum(num1: number|string , num2: number|string){
+    if (typeof num1 === 'string' || typeof num2 === 'string'){ // type guard
+        return +num1 + +num2;
+    }else{
+        return num1 + num2;
+    }
+}
 let result = findSum(10,20);
 console.log("result :"+result);
+
+/*let result3 = findTheSum(20,40);
+console.log(result3)*/
+
+
---
+++ tasks/4_functions.ts
@@ -0,0 +1,13 @@
+// //functions
+// //----------------------------------//
+//
+// //function findSum(num1: number,num2: number):number(return wenna ona type ek define krnawa){
+// //return num1+num2+"" (methanin define krnne return wenna ona type eka );
+// //return `sum is ${num1+num2}`; string type ek
+//
+// function findSum(num1: number,num2: number){
+//     return num1+num2;
+// }
+//
+// let result = findSum(10,20);
+// console.log("result :"+result);
