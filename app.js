4a0d97eccbe4cdde8d900a913a98ee48c6d9a5d3
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-12-02 11:10:52 +0530

    enum & heritage

--- app.js
+++ app.js
@@ -334,3 +334,67 @@ function updateTbl(){
         updateTbl();
     })
 }*/
+//union
+/*
+// union
+
+let array: (number | string | boolean)[] = [1, 2, "Hello", true];
+
+class Student {
+    id: number;
+    name: string;
+    college: string;
+    faculty: string;
+}
+
+class Teacher {
+    id: number;
+    name: string;
+    subject: string;
+    college: string;
+}
+
+class Soldier {
+    id: number;
+    name: string;
+    post: string;
+}
+*/
+/*
+//Enum
+
+enum  Roles{
+    SUPER_ADMIN,
+    ADMIN =1,
+    MANAGER,
+    SELLER,
+    BUYER
+}
+
+let myRole: Roles = Roles.ADMIN;
+
+function sendingMessages(role:Roles){
+    switch (role){
+        case Roles.SUPER_ADMIN:
+            console.log("Hello Super Admin");
+            break;
+        case Roles.ADMIN:
+            console.log("Admin");
+            break;
+        case Roles.MANAGER:
+            console.log("Hello Manager");
+            break;
+        case Roles.BUYER:
+            console.log("Hello Buyer");
+            break;
+        case Roles.SELLER:
+            console.log("Hello Seller");
+            break;
+    }
+}*/
+//heritage
+var person = /** @class */ (function () {
+    function person() {
+    }
+    return person;
+}());
